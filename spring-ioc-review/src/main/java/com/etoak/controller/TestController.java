0a34c8f9b1e4d1c92083f68b3a24c8f4a7271352
721902281 <48510571+721902281@users.noreply.github.com>
2020-12-20 17:14:38 +0800

    Update TestController.java
    协同

--- spring-ioc-review/src/main/java/com/etoak/controller/TestController.java
+++ spring-ioc-review/src/main/java/com/etoak/controller/TestController.java
@@ -10,5 +10,6 @@ public class TestController {
         System.out.println("测试3");
         System.out.println("合并master代码提交");
         System.out.println("本地代码1");
+        System.out.println("远程协同1");
     }
 }
